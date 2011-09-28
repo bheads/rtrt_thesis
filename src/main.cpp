dcfe287bdf5c6da5af2cf5565945c5236a72171a
Byron Heads <bheads@emich.edu>
2011-09-28 18:20:03 -0400

    added glog and gflags to project
    Logging and command line parsing added to main via log and gulags.

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,15 @@
  *  \date   July 14, 2011
  */
 
-
 #include "globals.h"
 
+// googles logger
+#include <glog/logging.h>
+// google gflags
+#include <gflags/gflags.h>
+
+/// proto-type for real main function
+int rtrt_main();
 
 /**
  *  \brief Program entry point.  
@@ -31,10 +37,54 @@
  */
 int main( int argc, char **argv )
 {
+    /// programs return code
+    int retcode = 1;
 
+    /// parse command line params
+    google::SetUsageMessage( "rtrt [options]" );
+    FLAGS_logtostderr = true;
+    google::ParseCommandLineFlags(&argc, &argv, true);
+
+    /// setup logger
+    google::InitGoogleLogging( argv[0] );
+    /// setup term handler
+    google::InstallFailureSignalHandler();
+
+    LOG(INFO) << "starting";
+
+    // call the real main function in a try catch to handle 
+    // uncaught exceptions.
+    try {
+        retcode = rtrt_main();
+    } catch( std::string str ) {
+        LOG( FATAL ) << "Uncaught string exception: " << str;
+    } catch( char *str ) {
+        LOG( FATAL ) << "Uncaught string exception: " << str;
+    } catch( int code ) {
+        LOG( FATAL ) << "Uncaught int exception: " << code;
+    }
+
+    LOG(INFO) << "finnished: returning " << retcode;
+
+    return( retcode );
+}
+
+
+/**
+ * \brief Programs real main function.
+ *
+ *  rtrt_main is the real main, it is called this way as a way to trap any
+ *  exceptions.  This functions creates all of the required objects and starts
+ *  the ray tracer.
+ *
+ *  \return Returns 0 on succes, non-zero on failure.
+ */
+int rtrt_main()
+{
 
 
 
     return( 0 );
 }
+
 
